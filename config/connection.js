f2f62ae5c2b6ad3ee4d8f396c49d5e0c2437a1e6
PrestonNguyen2001 <prestonnguyen2001@gmail.com>
2024-05-30 16:10:14 -0400

    fix

--- config/connection.js
+++ config/connection.js
@@ -9,9 +9,16 @@ const sequelize = process.env.DB_URL
       process.env.DB_PASSWORD,
       {
         host: process.env.DB_HOST,
-        dialect: "postgres",
+        dialect: process.env.DB_DIALECT || "postgres",
         port: process.env.DB_PORT || 5432,
       }
     );
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_URL:", process.env.DB_URL);
 
 module.exports = sequelize;
